0a0684004b5f1cf4746082a211b2c8ceeaa2a9af
Mukul <kmukul1197@gmail.com>
2019-12-08 15:09:29 +0530

    basic introduction

---
+++ student.rb
@@ -0,0 +1,4 @@
+#its a menu driven programme which will perform different task according to different user input
+puts "Enter number between 1-5, and the number has following tasks\n"
+puts "1. Show list of all student.\n2. Find student by roll.\n3. Find student by name.\n"
+puts "4. Add a new student.\n5. Close the programme.\n"
