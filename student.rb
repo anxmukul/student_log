026494e265911b31f074bf1dcbf1d7091d87798e
Mukul <kmukul1197@gmail.com>
2020-01-08 12:49:35 +0530

    adding name, roll no., class and sec

--- student.rb
+++ student.rb
@@ -1,16 +1,27 @@
+$name_array = []
+$roll_array = []
+$class_array = []
+$sec_array = []
 #its a menu driven programme which will perform different task according to different user input
 def add 
-    puts "Enter the number of student\n"
-    num = gets.chomp.to_i
-    student_array = []
-    puts "Enter name of each student\n"
-    num.times do
+    puts "Enter name of student\n"
+    name_array = []
         name = gets.chomp
-        student_array<<name
-    end
-    puts "\n"
-    puts student_array
+        name_array<<name
+    puts "Enter the roll no.\n"
+    roll_array = []
+        roll = gets.chomp
+        roll_array<<roll
+    puts "Enter the standard\n"
+    class_array = []
+        std = gets.chomp
+        class_array<<std
+    puts "Enter the sec\n"
+    sec_array = []
+        sec = gets.chomp
+        sec_array<<sec   
 end
+
 def search_roll
     puts "you are in search by roll function\n"
 end
@@ -21,6 +32,7 @@ end
 
 def list
     puts "here is the list of all student\n"
+    puts student_array
 end
 puts "Enter number between 1-5, and the number has following tasks\n"
 puts "1. Show list of all student.\n2. Find student by roll.\n3. Find student by name.\n"
@@ -35,10 +47,10 @@ elsif number == 2
     puts "You want to find student by roll\n"
     search_roll.call
 elsif number == 3
-    puts "you want to find student by name\n"
+    puts "You want to find student by name\n"
     search_name.call
 elsif number == 4
-    puts  "you want to add student\n"
+    puts  "You want to add student\n"
     add.call
 else 
     puts "Enter number between 1-5\n"
