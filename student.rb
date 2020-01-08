fe414c78d23691de9cdfd188cd90b26fd046495d
Mukul <kmukul1197@gmail.com>
2020-01-08 18:48:34 +0530

    Using .times method

--- student.rb
+++ student.rb
@@ -40,21 +40,23 @@ end
 puts "Enter number between 1-5, and the number has following tasks\n"
 puts "1. Show list of all student.\n2. Find student by roll.\n3. Find student by name.\n"
 puts "4. Add a new student.\n5. Close the programme.\n"
-puts "Enter any number\n"
-   
+
+10.times {
+puts "Enter any number\n"   
 number = gets.chomp.to_i            #taking input from user
 if number == 1
-    puts "You want list of all student\n"
+    #puts "You want list of all student\n"
     list.call
 elsif number == 2
-    puts "You want to find student by roll\n"
+    #puts "You want to find student by roll\n"
     search_roll.call
 elsif number == 3
-    puts "You want to find student by name\n"
+    #puts "You want to find student by name\n"
     search_name.call
 elsif number == 4
-    puts  "You want to add student\n"
+    #puts  "You want to add student\n"
     add.call
 else 
     puts "Enter number between 1-5\n"
 end
+}
