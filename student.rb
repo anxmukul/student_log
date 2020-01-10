5cab2dab53dd75c4df1f13a23a05f98c599edae1
Mukul <kmukul1197@gmail.com>
2020-01-10 12:37:20 +0530

    Using class and method

--- student.rb
+++ student.rb
@@ -1,57 +1,60 @@
 
 #its a menu driven programme which will perform different task according to different user input    
-def add 
-    student = Struct.new(:name, :roll, :std, :sec) do
-        def details
-        "Name: #{name}\nRoll no: #{roll}\nClass: #{std}\nSec: #{sec}"
+class Student
+    def add
+        @student = Struct.new(:name, :roll, :standard, :sec) do
+            def details
+                "Name: #{name}\nRoll no: #{roll}\nClass: #{std}\nSec: #{sec}"
+            end
         end
+            puts "Enter name of the student\n"
+            $Name = gets.chomp
+            puts "Enter the roll no of the student\n"
+            $Roll = gets.chomp
+            puts "Enter the standard of the student\n"
+            $Standard = gets.chomp
+            puts "Enter the section of the student\n"
+            $Section = gets.chomp
+            $a = @student.new("#{$Name}", "#{$Roll}", "#{$Standard}", "#{$Section}")
+        end
+ 
+    def list
+        puts $a.details
     end
-        puts "Enter name\n"
-        @Name = gets.chomp
-        puts "Enter roll\n"
-        @Roll = gets.chomp
-        puts "Enter standard\n"
-        @Standard = gets.chomp
-        puts "Enter section\n"
-        @Section = gets.chomp
-        a = student.new("#{@Name}", "#{@Roll}", "#{@Standard}", "#{@Section}") 
-        puts a.details 
+
+    def search_roll
+        puts "you are in search by roll function\n"
+    end
+        
+    def search_name
+        puts "you are in search by name function\n"
+    end
 
 end
 
-def search_roll
-puts "you are in search by roll function\n"
-end
-
-def search_name
-puts "you are in search by name function\n"
-end
-
-def list
-    puts "here is the list of all student\n"
-    #puts a.details
-end
 puts "Enter number between 1-5, and the number has following tasks\n"
-puts "1. Show list of all student.\n2. Find student by roll.\n3. Find student by name.\n"
-puts "4. Add a new student.\n5. Close the programme.\n"
+puts "1. Add a new student.\n2. Show list of all student.\n3. Find student by roll.\n"
+puts "4. Find student by name.\n5. Close the programme.\n"
 
-loop do             # initializing infinite loop of 1s time time interval
-puts "Enter any number\n"   
-    number = gets.chomp.to_i            #taking input from user and converting into integer
-if number == 1
-    #puts "You want list of all student\n"
-    list.call
-elsif number == 2
-    #puts "You want to find student by roll\n"
-    search_roll.call
-elsif number == 3
-    #puts "You want to find student by name\n"
-    search_name.call
-elsif number == 4
-    #puts  "You want to add student\n"
-    add.call
-else 
-    puts "Enter number between 1-5\n"
-end
-sleep (1)
+loop do
+    puts "Enter any number\n"
+    num = gets.chomp.to_i
+    if num == 1
+        object = Student.new
+        object.add        
+    elsif num == 2
+       object = Student.new
+       object.list
+    elsif num == 3
+        object = Student.new
+        object.search_roll
+        break
+    elsif num == 4
+        object = Student.new
+        object.search_name
+    elsif num == 5
+        break
+    end
+    sleep (1)
+
 end
